1095e6fc7c93677569d5b982a489c717326015ca
Vladyslav Kramarenko <vkramarenko.at.work@gmail.com>
2023-02-19 17:15:38 -0800

    specify port

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ def get_article():
 
 if __name__ == '__main__':
     # initialize_gmail_api()
-    api.run(host='0.0.0.0')
+    api.run(host='0.0.0.0', port=5000)
     #recipients = ['vkramarenko2711@gmail.com']
     #articles = db.collection('articles').stream()
     #for doc in articles:
